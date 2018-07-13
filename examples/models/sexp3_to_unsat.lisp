3e5544c04afc2dbad85480c621b3c4de16bc8a3d
Yoni Zohar <yoni206@gmail.com>
2018-07-12 21:09:42 -0700

    example

---
+++ examples/models/sexp3_to_unsat.lisp
@@ -0,0 +1,2 @@
+( e11 ( ((1) ( (utrc.test1.S1.n (utrc.test1.S1__impl.base_0 inst))))) )
+( e10 ( ((1.0) ( (utrc.test1.S1.o1 (utrc.test1.S1__impl.base_0 inst)))) ((1) ( (utrc.test1.S1.n (utrc.test1.S1__impl.base_0 inst)))) ((utrc.test1.S1.o1(utrc.test1.S1__impl.base_0(inst))) ( 1.0))) )
