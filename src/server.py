526cbe8db15bfe00460922a87261a72f65b49201
Yoni Zohar <yoni206@gmail.com>
2018-09-13 14:33:59 -0700

    server edit

--- src/server.py
+++ src/server.py
@@ -4,6 +4,16 @@ from reasoning_graph import *
 
 app = Flask(__name__)
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/shutdown', methods=['POST', 'GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
 @app.route('/solve_graph', methods=['POST'])
 def solve_reasoning_graph():
