6e717b4a6bc07bab161a2b93843dc60edc79433d
yoni206 <yoni206@gmail.com>
2019-08-06 22:04:05 -0700

    .

--- PI-Aug-2019-stats/scripts/gen_plots.py
+++ PI-Aug-2019-stats/scripts/gen_plots.py
@@ -7,7 +7,10 @@ def to_int(x):
     try:
         result = int(x)
     except:
-        return x
+        if x == "-":
+            return 0
+        else:
+            return x
     return result
 
 assert(len(sys.argv) >= 3)
@@ -16,7 +19,9 @@ destination_csv_dir = sys.argv[2]
 for csv_file in os.listdir(cluster_csv_dir):
     data = ps.read_csv(cluster_csv_dir + "/" + csv_file)
     data = data.applymap(to_int)
-    print(data)
+    virtual_best_column_name = data.columns[-1]
+    #data.sort_values(data.columns[-1],inplace=True)
+    print(data.dtypes)
     p = data.plot(kind='bar', x="DIRECTORY")
     f = p.get_figure()
     f.savefig('/home/yoniz/git/hermes/PI-Aug-2019-stats/plots/' + csv_file + '.png')
