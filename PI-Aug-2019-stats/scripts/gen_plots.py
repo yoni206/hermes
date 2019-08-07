7f162748316c0dbe4d6e17396c71643ea6bede84
yoni206 <yoni206@gmail.com>
2019-08-06 21:55:44 -0700

    .

--- PI-Aug-2019-stats/scripts/gen_plots.py
+++ PI-Aug-2019-stats/scripts/gen_plots.py
@@ -16,9 +16,7 @@ destination_csv_dir = sys.argv[2]
 for csv_file in os.listdir(cluster_csv_dir):
     data = ps.read_csv(cluster_csv_dir + "/" + csv_file)
     data = data.applymap(to_int)
-    #data.sort_values(data.columns[2], inplace=True)
-    #print(data
-    print(data.columns)
+    print(data)
     p = data.plot(kind='bar', x="DIRECTORY")
     f = p.get_figure()
     f.savefig('/home/yoniz/git/hermes/PI-Aug-2019-stats/plots/' + csv_file + '.png')
