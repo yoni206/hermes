90a235539d997795c4fad7e77a36183d7930b3a7
yoni206 <yoni206@gmail.com>
2019-08-12 23:27:10 -0700

    more solvers fix

--- dispatcher/dispatcher/solvers/comp/mathsat-20190601/bin/starexec_run_default.sh
+++ dispatcher/dispatcher/solvers/comp/mathsat-20190601/bin/starexec_run_default.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/yoniz/git/hermes/dispatcher/dispatcher/solvers/mathsat-20190601/bin/./mathsat $1
+/home/yoniz/git/hermes/dispatcher/dispatcher/solvers/comp/mathsat-20190601/bin/./mathsat $1
