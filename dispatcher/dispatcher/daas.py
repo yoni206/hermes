2d2fe45ce466790079410b0f456752ceadb83f11
yoni206 <yoni206@gmail.com>
2019-08-15 16:03:45 -0700

    lustre

--- dispatcher/dispatcher/daas.py
+++ dispatcher/dispatcher/daas.py
@@ -1,12 +1,15 @@
 import os
 import dispatcher
 import multiprocessing
+import subprocess
 from enum import Enum
+import json
 
 TMP_DIR="tmp"
 
 class LANG(Enum):
     SMTLIB = "smtlib"
+    LUSTRE = "lustre"
 
 class VerificationTask:
     def __init__(self):
@@ -26,7 +29,7 @@ class VerificationResult:
 
 #arg task is a VerificationTask
 #result is a VerificationResult
-def verify(task):
+def verify_smt(task):
     filename = task.id + ".smt2"
     if not os.path.exists(TMP_DIR):
         os.makedirs(TMP_DIR)
@@ -49,11 +52,39 @@ def verify(task):
     result.explanation = " ".join(disp_result_lines[1:])
     return result
 
+def verify_lustre(task):
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    solvers_dir = script_dir + "/solvers"
+    kind2_command = [solvers_dir + "/model_checkers/kind2", "-json"]
+
+    filename = task.id + ".LUS"
+    if not os.path.exists(TMP_DIR):
+        os.makedirs(TMP_DIR)
+    tmp_path = os.path.abspath(TMP_DIR + "/" + filename)
+    with open(tmp_path, "w") as f:
+        f.write(task.query)
+    kind2_command.append(tmp_path)
+    result_object = subprocess.run(kind2_command, stdout=subprocess.PIPE)
+    result_string = result_object.stdout.decode('utf-8')
+    #ignore warnings:
+    result_string = result_string[result_string.find("["):]
+    result_json = json.loads(result_string)
+    return result_json
+
+
+def verify(task):
+    if task.language == LANG.SMTLIB:
+        return verify_smt(task)
+    elif task.language == LANG.LUSTRE:
+        return verify_lustre(task)
+    else:
+        assert(False)
+
 def test1():
     with open("/home/yoniz/git/hermes/dispatcher/dispatcher/examples/bug.smt2", "r") as f:
         smtlib = f.read()
     task = VerificationTask()
-    task.id = "test"
+    task.id = "test1"
     task.query = smtlib
     task.language = LANG.SMTLIB
     task.solvers = ["all"]
@@ -65,12 +96,22 @@ def test2():
     with open("/home/yoniz/git/hermes/dispatcher/dispatcher/examples/simple/simple_get_value.smt2") as f:
         smtlib = f.read()
     task = VerificationTask()
-    task.id = "test"
+    task.id = "test2"
     task.query = smtlib
     task.language = LANG.SMTLIB
     task.solvers = ["generic"]
     result = verify(task)
     print(result)
 
+def test_json():
+    with open("/home/yoniz/git/hermes/dispatcher/dispatcher/examples/lustre/SW_agree.LUS") as f:
+        lustre = f.read()
+    task = VerificationTask()
+    task.id = "test_json"
+    task.query = lustre
+    task.language = LANG.LUSTRE
+    result = verify(task)
+    print(result)
+
 if __name__ == '__main__':
-    test1()
+    test_json()
